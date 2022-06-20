4b29bcece2b87231d8073ff67e2684edf2dacfcf
NamDao <bk.namapple@gmail.com>
2022-06-20 11:11:14 +0700

    init project

---
+++ Assets/Scripts/BallObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallObject : MonoBehaviour
+{
+    public int tubeId = -1;
+    public int indexInTube = -1;
+    public Image ballImage = null;
+}
---
+++ Assets/Scripts/TubeObject.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class TubeObject : MonoBehaviour, IPointerDownHandler
+{
+    public List<GameObject> posList = new List<GameObject>();
+    public GameObject posTop;
+    public GameObject ballList;
+    
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked pos: " + gameObject.name);
+    }
+}
